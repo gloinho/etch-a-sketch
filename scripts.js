c6350c8cfe2a63d976054bd7dd6e0e29772c0d4d
Anastacio <gl0inn@hotmail.com>
2022-06-20 10:57:15 -0300

    New feature - button to select grid size

--- scripts.js
+++ scripts.js
@@ -1,9 +1,34 @@
 const container = document.querySelector('#grid-container')
 for (i=1; i<=256; i++){
     let divs = document.createElement('div')
-    divs.classList.add('grid', i)
+    divs.classList.add('square')
     container.appendChild(divs)
 }
-
-const divs = document.querySelectorAll('.grid')
+let divs = document.querySelectorAll('.square')
 divs.forEach(div => div.addEventListener('mouseover', () => {div.style.background='black'}))
+
+
+const body = document.getElementById('body')
+const button = document.createElement('button')
+body.appendChild(button)
+
+const gridcontainer = document.getElementById('grid-container')
+
+function squareSize(size){
+    if(size>=100) return alert('Only values lower than 100!!!')
+    let sqsize = 500/size
+    return sqsize
+}
+button.addEventListener('click', () => {
+    let size = prompt('What size do you want?!')
+    let sqsize = squareSize(size)
+    gridcontainer.style.gridTemplateColumns = `repeat(${size}, ${sqsize}px)`
+    gridcontainer.style.gridTemplateRows = `repeat(${size}, ${sqsize}px)`
+    for (i=1; i<=size**2; i++){
+        let divs = document.createElement('div')
+        divs.classList.add('square')
+        container.appendChild(divs)
+    }
+    let divs = document.querySelectorAll('.square')
+    divs.forEach(div => div.addEventListener('mouseover', () => {div.style.background='black'}))
+})
